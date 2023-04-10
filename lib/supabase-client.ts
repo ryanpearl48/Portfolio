91d94c08091c495abc41ae1047844e6a631158ea
ryanpearl48 <ryanpearl98@gmail.com>
2023-04-10 14:22:46 -0700

    More supabase

---
+++ lib/supabase-client.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+import { Database } from '../types/supabase'
+
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_SUPABASE_ANON_KEY) {
+  throw new Error('Missing Supabase environment variables');
+}
+
+export const supabase = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_SUPABASE_ANON_KEY
+)
