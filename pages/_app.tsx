7a08048c5760ce9abf9b1cd0286c6f3364cdc83e
ryanpearl48 <ryanpearl98@gmail.com>
2023-04-11 08:45:51 -0700

    Added google auth, tailwind.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app'
-import { NextUIProvider } from '@nextui-org/react'
+import { NextUIProvider, createTheme } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
+
+
+const lightTheme = createTheme({
+  type: 'light',
+  // Change Colors
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+  // Change Colors
+})
 
 export default function App({ Component,
   pageProps,
@@ -13,13 +25,22 @@ export default function App({ Component,
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
   return (
-    <NextUIProvider>
-      <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-      >
-        <Component {...pageProps} />
-      </SessionContextProvider>
-    </NextUIProvider>
+    <NextThemesProvider
+    defaultTheme="system"
+    attribute="class"
+    value={{
+      light: lightTheme.className,
+      dark: darkTheme.className
+    }}
+    >
+      <NextUIProvider>
+        <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+        >
+          <Component {...pageProps} />
+        </SessionContextProvider>
+      </NextUIProvider>
+    </NextThemesProvider>
   )
 }
