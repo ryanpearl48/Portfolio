77f605e297c5f07232159118b89fcbe54f22e3f3
ryanpearl48 <ryanpearl98@gmail.com>
2023-04-17 08:29:57 -0700

    Updated about me

--- pages/about-me/index.tsx
+++ pages/about-me/index.tsx
@@ -1,14 +1,18 @@
 
 const AboutMe = () => {
   return (
-    <>
+    <div>
       <h1>Ryan Pearl</h1>
-      <ul>
-        <li>Where I'm From?</li>
-        <li>Prior Jobs</li>
-        <li>My time in the Air Force!</li>
-      </ul>
-    </>
+
+      <div>
+        <h2>A little bit about me</h2>
+          <p>I'm 24, from Michigan originally, but after joining the Air Force at 19 I spent some time in Texas and then the United Kingdom.
+            After my time in the U.K I moved back to the U.S and was stationed in Nevada, where I reside now. I really enjoyed my time in as
+            an Avionics Technician and am so happy to be taking this next step as a Software Developer.
+          </p>
+      </div>
+
+    </div>
   )
 }
 
