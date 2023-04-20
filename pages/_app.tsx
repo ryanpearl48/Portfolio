715c687d17b1f4a6cc34efb8b37038f4d1830ef8
ryanpearl48 <ryanpearl98@gmail.com>
2023-04-20 08:51:26 -0700

    Removed github update, and fixed SSR issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from 'next/app';
-import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { NextUIProvider, createTheme, theme } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 import { useState } from 'react';
-import { SSRProvider } from 'react-bootstrap';
+import { useSSR } from '@nextui-org/react';
 import Nav from '@/components/Nav';
 import '../styles/globals.css'
 
@@ -32,28 +32,29 @@ export default function App({ Component,
   initialSession: Session
 }>) {
 
+  const { isBrowser } = useSSR();
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
   return (
-  <SSRProvider>
-    <NextUIProvider>
-    <NextThemesProvider
-    defaultTheme="system"
-    attribute="class"
-    value={{
-      light: lightTheme.className,
-      dark: darkTheme.className
-    }}
-    >
-        <SessionContextProvider
-        supabaseClient={supabaseClient}
-        initialSession={pageProps.initialSession}
+    isBrowser && (
+       <NextUIProvider>
+        <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className
+        }}
         >
-          <Nav/>
-            <Component {...pageProps} />
-        </SessionContextProvider>
-    </NextThemesProvider>
-      </NextUIProvider>
-  </SSRProvider>
+            <SessionContextProvider
+            supabaseClient={supabaseClient}
+            initialSession={pageProps.initialSession}
+            >
+              <Nav/>
+                <Component {...pageProps} />
+            </SessionContextProvider>
+        </NextThemesProvider>
+       </NextUIProvider>
+    )
   )
 }
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@nextui-org/react'
+import { Link, Card, Table } from '@nextui-org/react'
 
 export const metadata = {
   title: 'Portfolio',
@@ -9,30 +9,89 @@ const Portfolio = () => {
   return (
     <div>
       <h1 className='relative flex justify-center items-center max-w-[500px] w-full m-auto pt-4'>Here's some links to my projects!</h1>
-
-      <div className='relative flex flex-col justify-between items-center max-w-[500px] w-full m-auto pt-4'>
-        <h1>AI Cookbook</h1>
-        <p>Myself and three teammates Ben Villafranca, Cody DiBella, and Jay Yuan created AI Cookbook
+      <Card className='relative flex flex-col justify-between items-center max-w-[500px] w-full m-auto pt-4'>
+        <Card.Header>AI Cookbook</Card.Header>
+        <Table
+      aria-label="Example table with static content"
+      css={{
+        height: "auto",
+        minWidth: "100%",
+      }}
+    >
+      <Table.Header>
+        <Table.Column>NAME</Table.Column>
+        <Table.Column>ROLE</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row key="1">
+          <Table.Cell>Ryan Pearl</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+        <Table.Row key="2">
+          <Table.Cell>Cody DiBella</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+        <Table.Row key="3">
+          <Table.Cell>Jay Yuan</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+        <Table.Row key="4">
+          <Table.Cell>Ben Villafranca</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+        <Card.Body>Myself and three teammates Ben Villafranca, Cody DiBella, and Jay Yuan created AI Cookbook
           Which utilizes Next.js, Supabase, MaterialUI, and Spoonacular/OpenAI API's.
           The main idea of the website is that you can take what ingredients you have around the house
           and you can input them into the search bar which will then have them added to your pantry.
           Once they're there they can get an AI generated recipe to utilize those previously inputed ingredients.
-        </p>
+        </Card.Body>
         <Link block underline isExternal href="https://aicookbook.app">
         AI CookBook
         </Link>
-      </div>
+      </Card>
 
-      <div className='relative flex flex-col justify-between items-center max-w-[500px] w-full m-auto pt-4'>
-        <h1>Vanity Vans</h1>
-        <p>This particular project is a mock e-commerce website. It utilizes Javascript, React, Sequelize, and some MUI.
+      <Card className='relative flex flex-col justify-between items-center max-w-[500px] w-full m-auto pt-4'>
+        <Card.Header>Vanity Vans</Card.Header>
+        <Table
+      aria-label="Example table with static content"
+      css={{
+        height: "auto",
+        minWidth: "100%",
+      }}
+    >
+      <Table.Header>
+        <Table.Column>NAME</Table.Column>
+        <Table.Column>ROLE</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row key="1">
+          <Table.Cell>Ryan Pearl</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+        <Table.Row key="2">
+          <Table.Cell>Cody DiBella</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+        <Table.Row key="3">
+          <Table.Cell>Christopher Kang</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+        <Table.Row key="4">
+          <Table.Cell>Ronnie Angeles</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+        <Card.Body>This particular project is a mock e-commerce website. It utilizes Javascript, React, Sequelize, and some MUI.
           We also incorportated stripe as a payment method option in this project. The majority of my responsibility was within authentication
           and admin pages and rights.
-        </p>
+        </Card.Body>
         <Link block underline isExternal href="https://vanityvans.onrender.com/">
         Vanity Vans
         </Link>
-      </div>
+      </Card>
 
 
     </div>
