0e6f0be2b211e15b112c2a94ba27486652c03986
ryanpearl48 <ryanpearl98@gmail.com>
2023-04-21 19:13:43 -0700

    small changes to about me page

--- pages/about-me/index.tsx
+++ pages/about-me/index.tsx
@@ -1,19 +1,9 @@
+import AboutMe from "@/components/AboutMe";
 
-const AboutMe = () => {
+const AboutMePage = () => {
   return (
-    <div>
-      <h1>Ryan Pearl</h1>
-
-      <div>
-        <h2>A little bit about me</h2>
-          <p>I'm 24, from Michigan originally, but after joining the Air Force at 19 I spent some time in Texas and then the United Kingdom.
-            After my time in the U.K I moved back to the U.S and was stationed in Nevada, where I reside now. I really enjoyed my time in as
-            an Avionics Technician and am so happy to be taking this next step as a Software Developer.
-          </p>
-      </div>
-
-    </div>
+    <AboutMe />
   )
 }
 
-export default AboutMe;
+export default AboutMePage;
