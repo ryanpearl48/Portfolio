026608cfed0f585ed53590704e71c9cc7bd31842
ryanpearl48 <ryanpearl98@gmail.com>
2023-05-04 13:49:56 -0700

    Added Sort Skeleton

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,12 +1,17 @@
 import Counter from '@/components/Counter'
 import React from 'react'
+import { Grid } from '@nextui-org/react'
+import Sort from '@/components/Sort'
 
 
 const Playground = () => {
   return (
     <>
       <h1>Here is basically an area where there's a ton of little coding things for practice!</h1>
-      <Counter />
+      <Grid>
+        <Counter />
+        <Sort />
+      </Grid>
     </>
   )
 }
