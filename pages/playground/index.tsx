2e819a4c4c5c0d6e61175bbec1570992e9db8715
ryanpearl48 <ryanpearl98@gmail.com>
2023-05-04 09:34:39 -0700

    Counter Skeleton Added

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,10 +1,12 @@
+import Counter from '@/components/Counter'
 import React from 'react'
-import { Card, Text, Link, Grid } from "@nextui-org/react";
+
 
 const Playground = () => {
   return (
     <>
       <h1>Here is basically an area where there's a ton of little coding things for practice!</h1>
+      <Counter />
     </>
   )
 }
